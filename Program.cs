50d9b2117703cb8941dc7080e7278472a867968a
Sidhant Malik <96983080+sidhant2094@users.noreply.github.com>
2025-07-01 15:40:37 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] mArray = new string[8];
+mArray[0] = "B123";
+mArray[1] = "C234";
+mArray[2] = "A345";
+mArray[3] = "C15";
+mArray[4] = "B177";
+mArray[5] = "G3003";
+mArray[6] = "C235";
+mArray[7] = "B179";
+
+foreach (string ids in mArray)
+{
+    if (ids.StartsWith("B"))
+    {
+        Console.WriteLine(ids);
+    }
+}
